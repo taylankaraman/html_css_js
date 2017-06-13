2432bf2f52ad84f60949d2cfc5c4c37e96f87608
Taylan Karaman <taylan.karaman@gmail.com>
2017-06-14 01:00:42 +1000

    Refactored duplicate code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var main = function(){
 	"use strict";
 
-	$(".comment-input button").on("click", function(event){
-		var $new_comment;
+	var addCommentFromInputBox = function () {
+		var $new_comment;	
 
 		if($(".comment-input input").val() !== ""){
 			$new_comment = $("<p>").text($(".comment-input input").val());
@@ -10,20 +10,16 @@ var main = function(){
 			$(".comments").append($new_comment);
 			$new_comment.fadeIn();
 			$(".comment-input input").val("");
-		}		
+		}			
+	}
+
+	$(".comment-input button").on("click", function(event){
+		addCommentFromInputBox();	
 	});
 
 	$(".comment-input input").on("keypress", function(event){
-		var $new_comment;
-
 		if(event.keyCode === 13){
-			if($(".comment-input input").val() !== ""){
-				$new_comment = $("<p>").text($(".comment-input input").val());
-				$new_comment.hide();
-				$(".comments").append($new_comment);
-				$new_comment.fadeIn();
-				$(".comment-input input").val("");
-			}			
+			addCommentFromInputBox();	
 		}
 	});
 };
