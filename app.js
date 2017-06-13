53cb85f891209b7419ea413b505336522e8214b9
Taylan Karaman <taylan.karaman@gmail.com>
2017-06-13 23:47:05 +1000

    Fixed null input box bug

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var main = function(){
 	"use strict";
 
 	$(".comment-input button").on("click", function(event){
-		var $new_comment = $("<p>").text($(".comment-input input").val());
+		var $new_comment;
 
-		$(".comments").append($new_comment);
+		if($(".comment-input input").val() !== ""){
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);
+		}		
 	});
 };
 
