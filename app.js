3ee6f0bf1a5463f6e0c95c6271b979aa0893e18a
Taylan Karaman <taylan.karaman@gmail.com>
2017-06-14 00:51:44 +1000

    Clear input box after comment is added. Enter key adds comment. FadeIn comments.

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ var main = function(){
 
 		if($(".comment-input input").val() !== ""){
 			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
 			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
 		}		
+	});
+
+	$(".comment-input input").on("keypress", function(event){
+		var $new_comment;
+
+		if(event.keyCode === 13){
+			if($(".comment-input input").val() !== ""){
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comments").append($new_comment);
+				$new_comment.fadeIn();
+				$(".comment-input input").val("");
+			}			
+		}
 	});
 };
 
